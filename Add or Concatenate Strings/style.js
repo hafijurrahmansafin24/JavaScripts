8d873f646b1e920f699d0aafc051ca5ba9e89bb6
Safin <jubayersafin4@gmail.com>
2025-09-17 23:32:58 +0600

    Commit

---
+++ Add or Concatenate Strings/style.js
@@ -0,0 +1,7 @@
+
+var firstName = "Jubayer";
+var lastName = " Safin.";
+var fullName = firstName + lastName;
+document.write(" My Name is " + fullName);
+
+document.write(" I am" + " 21" + " Years Old.");
