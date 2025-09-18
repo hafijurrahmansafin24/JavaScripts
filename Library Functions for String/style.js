4891353c4c6ed49b1f4306d6ca70cfb49fe6d6de
Safin <jubayersafin4@gmail.com>
2025-09-18 14:13:24 +0600

    Commit

---
+++ Library Functions for String/style.js
@@ -0,0 +1,25 @@
+
+var text = "Bangladesh";
+document.write("Number of charactor " + text.length + ".");
+
+var text = prompt("Enter your name : ");
+document.write(" Number of charactor " + text.length + ". ");
+
+var text = "Bangladesh";
+document.write(text.charAt(3) + ". ");
+
+var text = "Bangladesh";
+text = text.toUpperCase();
+document.write(text + ". ");
+
+var text = "Bangladesh";
+text = text.toLowerCase();
+document.write(text + ". ");
+
+var text1 = "Bangladesh";
+var text2 = " is a beautiful country"
+document.write(text1.concat(text2) + ". ");
+
+var text1 = "Bangladesh";
+var text = text1.slice(0, 3);
+document.write(text);
