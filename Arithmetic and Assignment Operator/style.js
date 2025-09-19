2580249b80c757845c84f25ea6da68b6dd37123a
Safin <jubayersafin4@gmail.com>
2025-09-19 23:40:43 +0600

    Commit

---
+++ Arithmetic and Assignment Operator/style.js
@@ -0,0 +1,53 @@
+//! Arithmetic Operator -> [+, -, *, /, %(Modulus), **(Exponent), ++, --]
+var x = 5;
+var x = x + 3;
+console.log(x);
+
+var x = 5;
+var x = x - 3;
+console.log(x);
+
+var x = 5;
+var x = x * 3;
+console.log(x);
+
+var x = 5;
+var x = x / 3;
+console.log(x);
+
+var x = 5;
+var x = x % 3;
+console.log(x);
+
+var x = 5;
+var x = x ** 3;
+console.log(x);
+
+//! Assignment Operator -> [=, +=, -=, *=, /=, %=, **=]
+var x = 3;
+x = 6;
+console.log(x);
+
+var x = 3;
+x += 6;
+console.log(x);
+
+var x = 3;
+x -= 6;
+console.log(x);
+
+var x = 3;
+x *= 6;
+console.log(x);
+
+var x = 3;
+x /= 6;
+console.log(x);
+
+var x = 3;
+x %= 6;
+console.log(x);
+
+var x = 3;
+x **= 6;
+console.log(x);
