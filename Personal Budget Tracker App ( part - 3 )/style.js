cd69adc196b1f34dfa02e14318eaf94556136fb9
Safin <jubayersafin4@gmail.com>
2025-09-20 00:57:26 +0600

    Commit

---
+++ Personal Budget Tracker App ( part - 3 )/style.js
@@ -0,0 +1,9 @@
+//! Personal Budget Tracker App Challenge (phase 3 - String Concatenation and Methods) -->>
+const userName = "Jubayer Safin";
+const income = 3750.50;
+const expense = 2400.25;
+
+console.log("Personal Budget Tracker App");
+console.log("User : " + userName.toUpperCase());
+console.log("Total Income : $" + income); // my income
+console.log("Total Expence : $" + expense); // my expense
