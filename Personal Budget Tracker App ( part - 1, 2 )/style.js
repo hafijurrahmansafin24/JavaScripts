17f444cfae592643ea1fe009dd50315ee0cb30b4
Safin <jubayersafin4@gmail.com>
2025-09-20 00:46:12 +0600

    Commit

--- Personal Budget Tracker App ( part - 1, 2 )/style.js
+++ Personal Budget Tracker App ( part - 1, 2 )/style.js
@@ -1,11 +1,11 @@
-//! Personal Budget Tracker App Challenge (Phase 1 - print with Consol)
+//! Personal Budget Tracker App Challenge (Phase 1 - print with Consol) -->>
 
 console.log("Personal Budget Tracker App");
 console.log("Jubayer Safin");
 console.log(3750.50); // my income
 console.log(2400.25); // my expense
 
-//! Personal Budget Tracker App Challenge (phase 2 - Variables, Data Types)
+//! Personal Budget Tracker App Challenge (phase 2 - Variables, Data Types) -->>
 
 const userName = "Jubayer Safin";
 const income = 3750.50;
