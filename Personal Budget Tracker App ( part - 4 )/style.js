3bed2b1fee7a50f853b6275bc84cae6fcc2b4db1
Safin <jubayersafin4@gmail.com>
2025-09-21 01:43:29 +0600

    Commit

---
+++ Personal Budget Tracker App ( part - 4 )/style.js
@@ -0,0 +1,34 @@
+//! Personal Budget Tracker App Challenge (phase 4 - Arithmetic Operators) -->>
+const userName = "Jubayer Safin";
+const income = 5000;
+
+//! Multiple Expenses -->>
+let rent = 1200;
+let food = 1000;
+let groceries = 600;
+let transport = 200;
+let entertainment = 300;
+
+//! Caltulate the Expenses -->>
+let totalExpenses = rent + food + groceries + transport + entertainment;
+
+//! Tax Deduction (10% of income) -->>
+let tax = income * 0.10;
+
+//! Net income after the tax -->>
+let netIncome = income - tax;
+
+//! Calculate remaining balance -->>
+let balance = netIncome - totalExpenses;
+
+//! Savings (20% of remaining balance) -->>
+let Savings = balance * 0.20;
+
+console.log("Personal Budget Tracker App");
+console.log("User : " + userName);
+console.log("Total Income : $" + income);
+console.log("Total Expenses : $" + totalExpenses);
+console.log("Tax Deduction (10% of income) : $" + tax);
+console.log("Net Income After Tax : $" + netIncome);
+console.log("Remaining Balance : $" + balance);
+console.log("Savings (20% of balance) : $" + Savings);
