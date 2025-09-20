5680870e43e383e0df22deb099b678ff091b8dda
Safin <jubayersafin4@gmail.com>
2025-09-21 00:57:28 +0600

    Commit

---
+++ Area Calculator/style.js
@@ -0,0 +1,14 @@
+var base = parseFloat(prompt("Enter Base = "));
+var height = parseFloat(prompt("Enter Height = "));
+
+var area = base * height;
+
+document.write("Area = " + area + "<br/>");
+
+
+var base = parseFloat(prompt("Enter Base = "));
+var height = parseFloat(prompt("Enter Height = "));
+
+var area = (base * height) / 2;
+
+document.write("Area = " + area);
