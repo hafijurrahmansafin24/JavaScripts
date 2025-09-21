e3417dc6cd2fc25d6fff9e492b969cd925adbd4d
Safin <jubayersafin4@gmail.com>
2025-09-22 01:43:08 +0600

    Commit

---
+++ Relational and Logical Operator/style.js
@@ -0,0 +1,22 @@
+//! Relational Operator -->> [>, >=, <, <=, ==, ===, !=, !==]
+var num1 = 20;
+var num2 = 25;
+
+console.log(num1>num2);
+console.log(num1>=num2);
+console.log(num1<num2);
+console.log(num1<=num2);
+console.log(num1==num2);
+console.log(num1===num2);
+console.log(num1!=num2);
+console.log(num1!==num2);
+
+//! Logical Operator -->> [&&, ||, !]
+var num1 = 50;
+var num2 = 30;
+var num3 = 20;
+
+console.log(num1>num2 && num1>num3);
+console.log(num1>num2 || num1>num3);
+console.log(!true);
+console.log(!false);
