bb9da5ac42b67d5c31ad985c6e39b90267d2b9a7
Safin <jubayersafin4@gmail.com>
2025-09-23 00:01:40 +0600

    Commit

---
+++ if, else if, else/style.js
@@ -0,0 +1,19 @@
+//! Even or Odd -->>
+var num = 5;
+
+if(num % 2 == 0)
+console.log("Even");
+
+else(num % 2 != 0)
+console.log("Odd");
+
+//! Positive or Negative Programmes -->>
+var num = prompt("Enter a number : ");
+if(num > 0)
+console.log("Positive");
+
+else if(num < 0)
+console.log("Negative");
+
+else
+console.log("Zero");
