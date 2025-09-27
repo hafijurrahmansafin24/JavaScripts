c79e43e9d4513a060cfa270cf70621bd9b5dfe8c
Safin <jubayersafin4@gmail.com>
2025-09-27 23:35:29 +0600

    Commit

---
+++ For Loop (part-1)/style.js
@@ -0,0 +1,29 @@
+//! For Loop -->>
+for(var x = 1; x <= 10; x++) {
+    document.write("<h1> Bangladesh </h1>");
+}
+document.write("<h1> End </h1>");
+
+
+for(var x = 1; x <= 100; x++) {
+    document.write(" " + x);
+}
+document.write("<h1> End </h1>");
+
+
+for(var x = 100; x >= 1; x--) {
+    document.write(" " + x);
+}
+document.write("<h1> End </h1>");
+
+
+for(var x = 1; x <= 99; x = x + 2) {
+    document.write(" " + x);
+}
+document.write("<h1> End </h1>");
+
+
+for(var x = 2; x <= 100; x = x + 2) {
+    document.write(" " + x);
+}
+document.write("<h1> End </h1>");
