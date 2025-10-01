2c36d6d80ea11e29f03995d3a76a11058aae4232
Safin <jubayersafin4@gmail.com>
2025-10-01 14:26:49 +0600

    Commit

---
+++ Traditional Function/style.js
@@ -0,0 +1,42 @@
+//! Traditional Function -->>
+
+//! creating a sqaure function -->>
+function sqaure(num) {
+    var result = num * num;
+    document.write("Result = " + result + "<br>");
+}
+
+//! Calling a function -->>
+sqaure(6);
+
+
+//! creating a addition function -->>
+function addition(x, y) {
+    var result = x + y;
+    document.write("Sum = " + result + "<br>");
+}
+addition(20, 10);
+
+
+//! creating a subtraction function -->>
+function subtraction(x, y) {
+    var result = x - y;
+    document.write("Sub = " + result + "<br>");
+}
+subtraction(30, 15);
+
+
+//! creating a multiply function -->>
+function multiply(num1, num2) {
+    var result = num1 * num2;
+    document.write("Multiply = " + result + "<br>");
+}
+multiply(6, 8);
+
+
+//! creating a division function -->>
+function division(num1, num2) {
+    var result = num1 / num2;
+    document.write("Division = " + result + "<br>");
+}
+division(100, 20);
