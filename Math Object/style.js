c744fb9da2ad84de737fb8d0c687f8212b4860c9
Safin <jubayersafin4@gmail.com>
2025-10-03 02:07:02 +0600

    Commit

---
+++ Math Object/style.js
@@ -0,0 +1,34 @@
+//! Math Object -->>
+Math.sqrt(25);
+
+Math.abs(-5);
+
+Math.sin(2);
+
+Math.cos(2);
+
+Math.tan(2);
+
+Math.pow(2, 4);
+
+Math.floor(2.4);
+
+Math.ceil(2.1);
+
+Math.round(3.4);
+
+Math.random();
+
+Math.random() * 7;
+
+Math.floor(Math.random() * 6);
+
+Math.floor(Math.random() * 5 + 1);
+
+Math.max(2, 5, -7);
+//! Maximum Object -->>
+var num1 = parseFloat(prompt("Enter a first number : "));
+var num2 = parseFloat(prompt("Enter a second number : "));
+
+var maximum = Math.max(num1, num2);
+console.log(maximum);
