2ec4f0079c2188ef88bbb4eeb65b78b0503865aa
Safin <jubayersafin4@gmail.com>
2025-10-11 01:41:01 +0600

    Commit

---
+++ Document Object Model (DOM)/style.js
@@ -0,0 +1,9 @@
+//! Document Object Model(DOM) -->>
+document.getElementById("heading1").innerHTML = "Hello";
+document.getElementById("heading2").innerHTML = "Good Bye";
+
+document.getElementsByTagName("h2")[0].innerHTML = "Hi";
+document.getElementsByTagName("h2")[1].innerHTML = "Hi";
+
+document.getElementsByClassName("para1")[0].innerHTML = "Hello";
+document.getElementsByClassName("para2")[0].innerHTML = "Bye";
