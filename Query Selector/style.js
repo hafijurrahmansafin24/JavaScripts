1578e5900c2bd7b8de7a998445b893aac36e9b69
Safin <jubayersafin4@gmail.com>
2025-10-12 00:02:05 +0600

    Commit

---
+++ Query Selector/style.js
@@ -0,0 +1,10 @@
+//! Query Selector -->>
+document.querySelector("#pid").innerHTML = "This is changed";
+
+document.querySelector(".pclass").innerHTML = "This is changed";
+
+document.querySelector("h1").innerHTML = "This is changed";
+
+document.querySelectorAll("h2")[0].innerHTML = "this is a text";
+
+document.querySelector("li a").innerHTML = "New Text";
