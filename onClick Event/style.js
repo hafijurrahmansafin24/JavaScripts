ef84da69f8cc161f5cf991e89d4693eabc752255
Safin <jubayersafin4@gmail.com>
2025-10-12 23:30:13 +0600

    Commit

---
+++ onClick Event/style.js
@@ -0,0 +1,23 @@
+//! onClick Event -->>
+
+var myVar = document.querySelector("#paraId");
+
+function myMessage1() {
+    myVar.innerHTML = "You have clicked on button1";
+}
+
+function myMessage2() {
+    myVar.innerHTML = "You have clicked on button2";
+}
+
+
+var myVar = document.querySelector("#myImageId");
+
+function myPicture1() {
+    myVar.src = "images/GUB Campus.jpg"
+}
+
+
+function myPicture2() {
+    myVar.src = "images/GUB Play Ground.jpg";
+}
