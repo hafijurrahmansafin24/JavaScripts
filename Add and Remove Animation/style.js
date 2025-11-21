0660065c8bed27a98ef54056d4ec9c3f1c2299ea
Safin <jubayersafin4@gmail.com>
2025-11-21 18:53:00 +0600

    Commit

---
+++ Add and Remove Animation/style.js
@@ -0,0 +1,39 @@
+for (var i = 0; i < 3; i++) {
+    document.querySelectorAll(".myButton")[i].addEventListener("click", function() {
+        var text = this.innerHTML;
+        console.log(text);
+        audioPlay(text);
+        playAnimation(text);
+    });
+}
+
+
+function audioPlay(text){
+    switch(text){
+            case 'A':
+                var audio = new Audio("sounds/heartbeat-sound-effect.mp3");
+                audio.play();
+                break;
+
+
+            case 'B':
+                var audio = new Audio("sounds/thud-sound-effect.mp3");
+                audio.play();
+                break;
+
+
+            case 'C':
+                var audio = new Audio("sounds/wow-sound-effect.mp3");
+                audio.play();
+                break;
+        }
+}
+
+function playAnimation(text) {
+    var selectedButton = document.querySelector("." + text);
+    selectedButton.classList.add("anim");
+
+    setTimeout(function () {
+        selectedButton.classList.remove("anim");
+    }, 1000);
+}
