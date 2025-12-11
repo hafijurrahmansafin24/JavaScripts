6d8911f43447c25fe2cb4e7a3d931a324782eb5e
Safin <jubayersafin4@gmail.com>
2025-12-12 02:08:41 +0600

    Commit

---
+++ BOM (Make a Clock)/style.js
@@ -0,0 +1,32 @@
+// setInterval() -->>
+const saveButton = document.querySelector(".save-btn");
+const message = document.querySelector(".message");
+
+
+saveButton.addEventListener("click", startClock);
+
+function startClock() {
+    let date = new Date();
+    let hours = date.getHours();
+    let minutes = date.getMinutes();
+    let seconds = date.getSeconds();
+
+    minutes = formetTime(minutes);
+    seconds = formetTime(seconds);
+
+    let time = hours + ":" + minutes + ":" + seconds;
+
+    
+
+    message.textContent = time;
+
+    setInterval(startClock, 1000);
+}
+
+
+function formetTime(value) {
+    if(value < 10) {
+        value = "0" + value;
+    }
+    return value;
+}
