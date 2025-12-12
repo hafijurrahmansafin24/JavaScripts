eb2dc8ea06ff434f6f93b54f7fafe38d28abee5d
Safin <jubayersafin4@gmail.com>
2025-12-12 21:07:53 +0600

    Commit

---
+++ Error Handle ( part - 2 )/style.js
@@ -0,0 +1,14 @@
+// Error handling --> throw -->>
+document.querySelector(".checkButton").addEventListener("click", function () {
+    var num = document.querySelector("#numTextfield").value;
+    
+    try {
+        if(num < 5) {
+            throw "input is too low";
+        } else if(num > 10) {
+            throw "input is too high";
+        }
+    } catch(error) {
+        console.log(error);
+    }
+});
