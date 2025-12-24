17ba12bdd7570c3814efd52e7a78dbdb47358e17
Safin <jubayersafin4@gmail.com>
2025-12-24 17:31:28 +0600

    Commit

---
+++ ES6(String Methods)/style.js
@@ -0,0 +1,11 @@
+//! ES6(String Methods) -->>
+const message = "Today is friday";
+
+// startsWith Methods -->>
+console.log(message.startsWith('Today'));
+
+// endsWith Methods -->>
+console.log(message.endsWith('friday'));
+
+// includes Methods -->>
+console.log(message.includes('is'));
